b429540e7f905fb4514e99b5fc5b075706bea3fe
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2019-12-16 16:44:25 -0500

    completed steps for setting up navigation, finished project

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
+        {/* setting up Navigation using <Link/> */}
         <div>
-          <a href="">Home</a>
+          <Link to="/">
+            Home
+          </Link>
+          {/* <a href="">Home</a> */}
         </div>
         <div>
-          <a href="">About</a>
+          <Link to="/about">
+            About
+          </Link>
+          {/* <a href="">About</a> */}
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link to="/contact">Contact</Link>
+          {/* <a href="">Contact</a> */}
         </div>
       </div>
     </div>
